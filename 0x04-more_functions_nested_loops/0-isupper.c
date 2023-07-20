01727dea539eed54e5d95dfa3616b1e4488d1fa8
Onochielilian <onochielilian18@gmail.com>
2023-07-20 21:18:34 +0100

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,12 @@
 /**
 * _isupper - checks for uppercase character
 * @c: the character to be checked
-* Return: 1 if c is uppercase, 0 otherwise
+* Return: 0 or 1
 */
 int _isupper(int c)
 {
-return (c >= 'A' && c <= 'Z');
+if (c >= 'A' && c <=  'Z')
+return (1);
+else
+return (0);
 }
