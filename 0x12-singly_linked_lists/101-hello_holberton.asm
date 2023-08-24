1c2212567894a6755e62ebc9b3b52e6a78f264ae
Onochielilian <onochielilian18@gmail.com>
2023-08-24 23:45:52 +0100

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,9 @@
-extern	printf
-
-SECTION .data
-
-message db "Hello, Holberton", 10
-
-SECTION .text
-
-global main
+global    main
+          extern    printf
 main:
-mov edi, message
-mov eax, 0
-call printf
-mov rax, 0
-ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
